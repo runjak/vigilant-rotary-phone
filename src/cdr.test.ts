9e7a35a47418aacbb2cd547069b6360aaeb15ccf
Jakob Runge <sicarius@g4t3.de>
2018-02-04 16:54:00 +0100

    Add test for cdr
    - Also fixes cdr

---
+++ src/cdr.test.ts
@@ -0,0 +1,23 @@
+import cdr, { ERROR_NO_CDR_FOR_SYMBOL } from './cdr';
+
+describe('cdr', () => {
+  it('should return ERROR_NO_CDR_FOR_SYMBOL given a Symbol', () => {
+    const actual = cdr(Symbol('wtf'));
+
+    expect(actual).toBe(ERROR_NO_CDR_FOR_SYMBOL);
+  });
+
+  it('should return the tail of an array', () => {
+    const cases = [
+      { given: [], expected: []},
+      { given: ['foo'], expected: []},
+      { given: [2,3,5,7], expected: [3,5,7]},
+    ];
+
+    cases.forEach((c) => {
+      const { given, expected } = c;
+
+      expect(cdr(given)).toEqual(expected);
+    });
+  });
+});
--- src/cdr.ts
+++ src/cdr.ts
@@ -3,11 +3,11 @@ import { Expression } from './index';
 export const ERROR_NO_CDR_FOR_SYMBOL = Symbol.for('ERROR_NO_CDR_FOR_SYMBOL');
 
 const cdr = (xs: Expression): Expression => {
-  if(xs instanceof Symbol) {
+  if(typeof xs === 'symbol') {
     return ERROR_NO_CDR_FOR_SYMBOL;
   }
 
-  const [x, ...rest] = xs;
+  const [x, ...rest] = xs as Array<Expression>;
   return rest;
 };
 
