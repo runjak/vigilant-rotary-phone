60e4a2f6015ec4c2a0188795f5121975fc9e4c9c
Jakob Runge <sicarius@g4t3.de>
2018-02-05 23:28:13 +0100

    Add FIXME.md and types to expressions/

---
+++ src/expressions/FIXME.md
@@ -0,0 +1,2 @@
+Tests here are complicated and will need to wait a bit.
+This is because they depend on `evaluation` being ready.
---
+++ src/expressions/types.ts
@@ -0,0 +1,5 @@
+import { Expression } from '../types';
+
+export type ExpressionDefinition = {
+  [name: string]: Expression,
+};
