41cd09fa57dfeefe91983a143cf9cb3779c68b02
Jakob Runge <sicarius@g4t3.de>
2018-02-11 22:33:36 +0100

    Add primitives/index.ts
    - This module gather the FunctionDefinitions for primitives

---
+++ src/primitives/index.ts
@@ -0,0 +1,16 @@
+import { FunctionDefinitions } from '../types';
+import atom from 'atom';
+import car from 'car';
+import cdr from 'cdr';
+import cons from 'cons';
+import eq from 'eq';
+import quote from 'quote';
+
+export default {
+  atom,
+  car,
+  cdr,
+  cons,
+  eq,
+  quote,
+} as FunctionDefinitions;
--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,10 @@ export interface Cons extends Array<Expression> {};
 
 export type EvalFunction = (e: Expression) => Expression;
 
+export type FunctionDefinitions = {
+  [name: string]: (...expressions: Array<Expression>) => Expression,
+};
+
 export type ExpressionDefinition = {
   [name: string]: Expression,
 };
