cd53908111c606339c249fa386beaaf94af2bbdd
Jakob Runge <sicarius@g4t3.de>
2018-07-15 13:19:58 +0200

    Harden stringify

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,8 +1,16 @@
 import { Expression, Cons } from './types';
 
 const stringify = (e: Expression): string => {
+  if (!e) {
+    return '_';
+  }
+
   if (typeof e === 'symbol') {
     return Symbol.keyFor(e) as string;
+  }
+
+  if (typeof e === 'function') {
+    return String(e);
   }
 
   const parts = (e as Cons).map(stringify);
